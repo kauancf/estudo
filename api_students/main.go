88c9358c74051f7947666246a5d79923de67b603
Kauan Cácere Ferreira <kauanre@hotmail.com>
2024-07-31 23:08:03 -0300

    Creation of the first route

---
+++ api_students/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/students", getStudents)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handlerb
+func getStudents(c echo.Context) error {
+	return c.String(http.StatusOK, "List of all students")
+}
