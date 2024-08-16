b19684ab22f04d34fd9224b68a1c1abf856c1968
Kauan Cácere Ferreira <kauanre@hotmail.com>
2024-08-16 19:00:37 -0300

    request creation

--- api_students/api/handler.go
+++ api_students/api/handler.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/kauancf/estudo/tree/main/api_students/schemas"
 	"github.com/labstack/echo"
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
@@ -26,9 +27,23 @@ func (api *API) getStudents(c echo.Context) error {
 }
 
 func (api *API) creatStudents(c echo.Context) error {
-	student := schemas.Student{}
-	if err := c.Bind(&student); err != nil {
+	studentReq := StudentRequest{}
+	if err := c.Bind(&studentReq); err != nil {
 		return err
+	}
+
+	if err := studentReq.Validate(); err != nil {
+		log.Error().Err(err).Msgf("[api] error validating struct")
+		return c.String(http.StatusBadRequest, "Error validating  Student")
+
+	}
+
+	student := schemas.Student{
+		Name:   studentReq.Name,
+		CPF:    studentReq.CPF,
+		Email:  studentReq.Email,
+		Age:    studentReq.Age,
+		Active: *studentReq.Active,
 	}
 
 	if err := api.DB.AddStudent(student); err != nil {
