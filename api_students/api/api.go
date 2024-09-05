242adec35c3e6d09b11b08b4dcbd77264b1cd388
Kauan Cácere Ferreira <kauanre@hotmail.com>
2024-09-05 13:07:32 -0300

    Swagger creation for documentation

--- api_students/api/api.go
+++ api_students/api/api.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"github.com/kauancf/estudo/tree/main/api_students/db"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/kauancf/estudo/tree/main/api_students/docs"
 )
 
 type API struct {
@@ -11,6 +14,12 @@ type API struct {
 	DB   *db.StudentHandler
 }
 
+// @title Student API
+// @version 1.0
+// @description This is a sample server Student API
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func NewServer() *API {
 	// Echo instance
 	e := echo.New()
@@ -37,6 +46,7 @@ func (api *API) ConfigureRoutes() {
 	api.Echo.GET("/students/:id", api.getStudent)
 	api.Echo.PUT("/students/:id", api.updateStudent)
 	api.Echo.DELETE("/students/:id", api.deleteStudent)
+	api.Echo.GET("/swagger/*", echoSwagger.WrapHandler)
 }
 
 func (api *API) Start() error {
